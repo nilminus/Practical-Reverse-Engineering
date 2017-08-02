de96849af7c48384db62d776f8172f95fcf7e129
Nil Minus <nilminus@gmail.com>
2017-08-02 17:51:24 +0300

    Completed mystery5

---
+++ 2. ARM/mystery5.c
@@ -0,0 +1,26 @@
+char* unk_function(int number){
+
+    switch(number){
+        case 6: {
+            return "E";
+            break;
+        }
+        case 7: {
+            return "D";
+            break;
+        }
+        case 8: {
+            return "C";
+            break;
+        }
+        case 9: {
+            return "B";
+            break;
+        }
+        default: {
+            return "E";
+            break;
+        }
+    }
+
+}
